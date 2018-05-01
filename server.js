9e626f3846550c3f6639e57a7f088f72eb463d47
aksoyludeniz <aksoyludeniz@outlook.com>
2018-04-30 20:42:19 -0400

    heroku database

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/zinedtea")
+  process.env.MONGODB_URI || "mongodb://teas:teas@ds263619.mlab.com:63619/heroku_4hzsjhs5")
 
 
 
